a9e3295d39496b08825f15de312d81e90bace552
Brian Kotos <brian.kotos@gmail.com>
2018-11-04 15:28:43 -0500

    Create separate development and production builds.

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,38 @@ const webpack = require('webpack');
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-module.exports = {
-    devtool: 'inline-source-map',
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
+module.exports = (env, options) => {
+    let isProduction = options.mode === 'production';
+
+    let devtool = '';
+    if (!isProduction) {
+        devtool = 'inline-source-map';
+    }
+
+    return {
+        devtool,
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.ts'],
+            plugins: [new TsconfigPathsPlugin()]
+        },
+        target: 'node',
+        entry: './src/index.ts',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'index.js'
+        },
+        plugins: [
+            // Adds a shebang to the top of output files.  This is necessary since we're developing a cli application.
+            new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })
         ]
-    },
-    resolve: {
-        extensions: ['.ts'],
-        plugins: [new TsconfigPathsPlugin()]
-    },
-    target: 'node',
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
-    },
-    plugins: [
-        // Adds a shebang to the top of output files.  This is necessary since we're developing a cli application.
-        new webpack.BannerPlugin({ banner: '#!/usr/bin/env node', raw: true })
-    ]
+    };
 };
