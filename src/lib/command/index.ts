fe50f5d026afd34cf3c749e9e7685be456e23c11
Brian Kotos <brian.kotos@gmail.com>
2021-09-11 13:43:22 -0400

    Version command

--- src/lib/command/index.ts
+++ src/lib/command/index.ts
@@ -4,7 +4,9 @@ import { CliPrompter } from "lib/cli"
 import { Merger } from "lib/env/merger"
 import { ProcessDependencies as OptionsProcessDependencies } from "../options"
 import help from './help'
+import version from './version'
 import merge from './merge'
+import { PackageJsonReader } from "lib/package-json"
 
 // TODO rename ProcessDependencies?
 export type ProcessDependencies = Pick<NodeJS.Process, 'exit'> & OptionsProcessDependencies
@@ -18,16 +20,19 @@ interface CliDependencies {
     merger: Merger
     cliPrompter: CliPrompter
     process: ProcessDependencies
+    packageJsonReader: PackageJsonReader
 }
 
-export default async ({ merger, cliPrompter, process }: CliDependencies) => {
+export default async ({ merger, cliPrompter, process, packageJsonReader }: CliDependencies) => {
     const [,,command] = process.argv
 
     const isHelpCommand = command === 'help' || command === '--help'
-    if (isHelpCommand) return help(console, fs, path)
+    if (isHelpCommand) return help(console, packageJsonReader)
 
     const isVersionCommand = command === 'version' || command === '--version' || command === '-v'
-    if (isVersionCommand) return // TODO implement this
+    if (isVersionCommand) return version(console, packageJsonReader)
 
     await merge(merger, cliPrompter, process)
+    // TODO add warning here if not using "merge" command
+    // TODO add error here if unknown command
 }
